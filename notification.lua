c32facb6799c6891a1691771580e770927e266b6
ZeoHub <romanozchristian0@gmail.com>
2025-06-22 02:40:44 +0800

    Update notification.lua

--- notification.lua
+++ notification.lua
@@ -1,12 +1,2 @@
-local function SafeRequire(url)
-    local ok, result = pcall(function()
-        local src = game:HttpGet(url)
-        return loadstring(src)()
-    end)
-    if not ok then
-        warn("Failed to load script from "..url..": "..tostring(result))
-    end
-end
-
-SafeRequire("https://raw.githubusercontent.com/ZeoHub/GrowAGardenV3/refs/heads/main/Nothing/Pet%20Placer.lua")
-SafeRequire("https://raw.githubusercontent.com/DeltaGay/femboy/refs/heads/main/GardenSpawner.lua")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZeoHub/GrowAGardenV3/refs/heads/main/Nothing/Pet%20Placer.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DeltaGay/femboy/refs/heads/main/GardenSpawner.lua"))()
