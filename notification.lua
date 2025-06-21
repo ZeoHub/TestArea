53e0fcb784652da8392595eee28e021727f8eeb4
ZeoHub <romanozchristian0@gmail.com>
2025-06-22 02:41:27 +0800

    Update notification.lua

--- notification.lua
+++ notification.lua
@@ -1,2 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ZeoHub/GrowAGardenV3/refs/heads/main/Nothing/Pet%20Placer.lua"))()
+local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZeoHub/GrowAGardenV3/refs/heads/main/Nothing/Pet%20Placer.lua"))()
+Spawner.Load()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DeltaGay/femboy/refs/heads/main/GardenSpawner.lua"))()
