438537846977a2380f6c395d3ba94eef83c49efe
ZeoHub <romanozchristian0@gmail.com>
2025-06-21 17:22:22 +0800

    Update petspawnermobile1.lua

--- petspawnermobile1.lua
+++ petspawnermobile1.lua
@@ -1,5 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ZeoHub/TestArea/refs/heads/main/petplacer.lua"))()
-local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZeoHub/GrowAGardenV3/refs/heads/main/Visual/Pet%20Spawner.lua"))()
+local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/DeltaGay/femboy/refs/heads/main/GardenSpawner.lua"))()
 pcall(function() game.Players.LocalPlayer.PlayerGui.PetSpawnerUI:Destroy() end)
 
 local function Create(class, props)
